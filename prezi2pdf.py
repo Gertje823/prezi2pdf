0f2e756edf6db2a880279003a1cbd47a8cd8eb80
Gertje823 <36937387+Gertje823@users.noreply.github.com>
2022-08-18 20:26:41 +0200

    Now also supporting Prezi Videos
    Now also supporting Prezi Videos

--- prezi2pdf.py
+++ prezi2pdf.py
@@ -1,17 +1,65 @@
 import requests
-import re
+import re, os, json
 from img2pdf import convert
+import yt_dlp
+import argparse
 
-input_url = input("Enter prezi url: ")
-id = re.findall('([0-z]{12})', input_url)[0]
 
-url = f"https://prezi.com/api/v2/storyboard/frames/{id}/"
-data = requests.get(url).json()
-content = []
-i = 0
-for frame in data['frames']:
-    r = requests.get(frame['images'][0]['urls']['png'])
-    content.append(r.content)
-    i+=1
-with open(f'{id}.pdf', 'wb') as pdf:
-    pdf.write(convert(content))
+# ArgParse
+parser = argparse.ArgumentParser(description='Download Prezi Presentations and Videos')
+parser.add_argument('--url','-u',dest='url', action='store', help='Prezi URL', required=True)
+parser.add_argument('--download-json','-j',dest='download_json', action='store_true', help='Download JSON file', required=False)
+
+args = parser.parse_args()
+
+
+def download_video(id):
+    
+    url = f"https://prezi.com/api/v5/presentation-content/{id}/"
+    data = requests.get(url).json()
+    try:
+        os.mkdir(f"./videos")
+    except FileExistsError:
+        pass
+    print(f"Downloading {data['meta']['title']}")
+    video_url = data['meta']['video_signed_url_with_title']
+    ydl = yt_dlp.YoutubeDL({'outtmpl': f'./videos/{id}.%(ext)s',
+                            'merge_output_format': 'mp4', 'ignoreerrors': 'True',
+                            'writethumbnail': 'True', 'retries': 100,
+                            'add-header': 'user-agent:Mozilla/5.0',})
+    info_dict = ydl.extract_info(video_url, download=True)
+    if args.download_json:
+        with open(f"./videos/{id}.json", 'w') as outfile:
+            outfile.writelines(json.dumps(data, indent=4))
+
+
+
+def download_presentation(id):
+    url = f"https://prezi.com/api/v2/storyboard/frames/{id}/"
+    data = requests.get(url).json()
+    content = []
+    i = 0
+    total = len(data['frames'])
+    for frame in data['frames']:
+        r = requests.get(frame['images'][0]['urls']['png'])
+        content.append(r.content)
+        print(f"Downloading slide {i+1}/{total}")
+        i += 1
+    with open(f'{id}.pdf', 'wb') as pdf:
+        pdf.write(convert(content))
+
+id = re.findall('([0-z]{12})', args.url)[0]
+
+if "prezi.com/v/" in args.url:
+    download_video(id)
+
+elif "prezi.com/i/" in args.url:
+    print("Prezi design not supported yet")
+
+elif "prezi.com/" in args.url:
+    print("Prezi URL not recognized. Check your url or open an issue.")
+else:
+    print("Please provide a valid prezi URL")
+
+
+
